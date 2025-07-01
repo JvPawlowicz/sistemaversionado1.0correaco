1bcd2b3e9dbdc3293e07a3189fee6b392a6e99e3
João Victor Gonzalez Pawlowicz <joaovictorgpawlowicz@gmail.com>
2025-07-01 17:00:41 +0000

    eu não estou conseguindo dar deploy no sistema

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "i.pravatar.cc",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "firebasestorage.googleapis.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+  typescript: {
+    // Disallow building with TypeScript errors
+    ignoreBuildErrors: false,
   },
+  eslint: {
+    // Disallow building with ESLint errors
+    ignoreDuringBuilds: false,
+  }
 };
 
 export default nextConfig;
