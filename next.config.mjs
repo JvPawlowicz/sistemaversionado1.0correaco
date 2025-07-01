8371895466131b58c937e9d11a9d39bffddbcf3a
João Victor Gonzalez Pawlowicz <joaovictorgpawlowicz@gmail.com>
2025-07-01 19:46:48 +0000

    ainda estamos com erro no backend e no publish

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+      }
+    ],
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
