c8576a4cb5f6ffedf6c16f6dd5ea8baa3bc8008e
João Victor Gonzalez Pawlowicz <joaovictorgpawlowicz@gmail.com>
2025-07-02 10:36:00 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,47 @@
-/** @type {import('next').NextConfig} */
+
+// @ts-check
+import { withSentryConfig } from '@sentry/nextjs';
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  /* Your Next.js config */
   typescript: {
-    // Allows production builds to successfully complete even if
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
+    // !! WARN !!
     ignoreBuildErrors: true,
   },
 };
 
-// Injected by Sentry CLI - this is a temporary workaround for Vercel builds
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withSentryConfig } = require("@sentry/nextjs");
+const sentryConfig = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during build
+  silent: true,
+  org: process.env.SENTRY_ORG,
+  project: process.env.SENTRY_PROJECT,
+};
+
+const sentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+  // Hides source maps from generated client bundles
+  hideSourceMaps: true,
+
+  // Tunnels sentry requests through this app's domain to avoid ad-blockers
+  tunnelRoute: '/monitoring',
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
+};
 
 export default withSentryConfig(
   nextConfig,
-  {
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options
-
-    // Suppresses source map uploading logs during build
-    silent: true,
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-  },
-  {
-    // For all available options, see:
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-    // Hides source maps from generated client bundles
-    hideSourceMaps: true,
-
-    // Automatically tree-shake Sentry logger statements to reduce bundle size
-    disableLogger: true,
-    
-    // Enables automatic instrumentation of Vercel Cron Monitors.
-    automaticVercelMonitors: true,
-  }
+  sentryConfig,
+  sentryWebpackPluginOptions
 );
