753d4e502c3c26937957100fa75c8659d1580f2d
João Victor Gonzalez Pawlowicz <joaovictorgpawlowicz@gmail.com>
2025-07-02 10:48:47 +0000

    Continue, faça novamente o plano de ação que nós desenvolvemos

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // Your existing configuration goes here
-    // Note: The Sentry 'withSentryConfig' wrapper is no longer required in this file.
-    // The Sentry SDK is now initialized in the 'instrumentation.ts' file.
+  experimental: {
+    instrumentationHook: true,
+  },
 };
 
 export default nextConfig;
